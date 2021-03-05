19b65b51e0e1726954ee5539ddd1b2c3fb3d8f18
João Henrique Ferreira <jaycops@gmail.com>
2021-03-04 21:32:03 -0300

    criado arquivo de terminal

---
+++ terminal.py
@@ -0,0 +1,8 @@
+path_exibido = "->%/"
+while True:
+    comando = input(path_exibido)
+
+# precisamos armazenar os estados, quais ?
+# O path atual
+# Quais arquivos existem
+# O conteúdo dos arquivos
